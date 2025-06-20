6ed1781e5af17705971bce44fbb8a4c8973bdff9
WellyXY <welly.xy@gmail.com>
2025-06-20 00:08:19 -0700

    修正端口設置為 3080

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const fs = require('fs-extra');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3080;
 
 // 中間件
 app.use(cors());
